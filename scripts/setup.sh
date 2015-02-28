15ee45d926ff59696490bf403ddc3c2521b837a6
Paul Glass <paul.glass@rackspace.com>
2015-02-28 23:21:54 +0000

    Update scripts
    * setup.sh is better
    * chmod +x the scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt-get -y update
 
-sudo apt-get install git python-dev python-pip python-virtualenv ntp gnuplot
+sudo apt-get -y install git python-dev python-pip python-virtualenv ntp gnuplot
 
 pip install -U pip
 hash -r
 
+git clone https://github.com/pglass/digaas.git
+cd digaas
 virtualenv --no-site-packages .venv
 . .venv/bin/activate
 
 pip install uwsgi Cython falcon gevent dnspython redis
+
+echo "digaas setup complete."
+echo "To configure digaas:"
+echo "  - copy digaas_config.py.sample to digaas_config.py"
+echo "  - update digaas_config.py "
+echo "To start digaas:"
+echo "  . .venv/bin/activate"
+echo "  ./bin/digaas.sh &"
+echo "REMEMBER TO SYNC TO NETWORK TIME"
